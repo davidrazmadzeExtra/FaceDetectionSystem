d4399a8e0f4ad74dafa24761713207a65d09c6e6
David Razmadze <razd45@gmail.com>
2022-08-27 10:54:45 -0400

    Initial Commit

---
+++ FaceDetectionSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceDetectionSystem
+//
+//  Created by David Razmadze on 8/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
